85dec55b32ce9e116a927cd75b3e8f1f7fc1ded3
Mathieu Corti <mathieucorti@gmail.com>
2018-04-30 17:00:39 +1000

    Created the base project

---
+++ cmake-build-debug/CMakeFiles/IslandDefense3D.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/IslandDefense3D.dir/main.cpp.o"
+  "IslandDefense3D.pdb"
+  "IslandDefense3D"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/IslandDefense3D.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
