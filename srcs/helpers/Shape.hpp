f57d1f139df9730ed25e68e33d5242a854b6b232
Mathieu Corti <mathieucorti@gmail.com>
2018-05-01 12:26:50 +1000

    Clean config and prepared shape for 3D objects

--- srcs/helpers/Shape.hpp
+++ srcs/helpers/Shape.hpp
@@ -22,15 +22,16 @@ class Shape;
 typedef std::list<Shape> Shapes;
 
 struct Coordinates {
-  Coordinates(float x, float y) : x(x), y(y) {}
+  Coordinates(float x, float y, float z) : x(x), y(y), z(z) {}
 
   float x;
   float y;
+  float z;
 };
 
 struct BoundingBox {
-  BoundingBox(const Coordinates &upperLeft, const Coordinates &lowerRight) : upperLeft(upperLeft),
-                                                                             lowerRight(lowerRight) {}
+//  BoundingBox(const Coordinates &upperLeft, const Coordinates &lowerRight) : upperLeft(upperLeft),
+//                                                                             lowerRight(lowerRight) {}
 
   Coordinates upperLeft;
   Coordinates lowerRight;
@@ -64,42 +65,42 @@ public:
   explicit Shape(const float &deltaX, const float &deltaY, std::vector<Coordinates> parts = std::vector<Coordinates>(),
                  GLenum mode = GL_POLYGON, Color color = Color(1, 1, 1));
 
-  BoundingBox getBoundingBox() const {
-    auto xExtremes = std::minmax_element(parts.begin(), parts.end(),
-                                         [](const Coordinates &lhs, const Coordinates &rhs) {
-                                           return lhs.x > rhs.x;
-                                         });
-    auto yExtremes = std::minmax_element(parts.begin(), parts.end(),
-                                         [](const Coordinates &lhs, const Coordinates &rhs) {
-                                           return lhs.y > rhs.y;
-                                         });
+//  BoundingBox getBoundingBox() const {
+//    auto xExtremes = std::minmax_element(parts.begin(), parts.end(),
+//                                         [](const Coordinates &lhs, const Coordinates &rhs) {
+//                                           return lhs.x > rhs.x;
+//                                         });
+//    auto yExtremes = std::minmax_element(parts.begin(), parts.end(),
+//                                         [](const Coordinates &lhs, const Coordinates &rhs) {
+//                                           return lhs.y > rhs.y;
+//                                         });
+//
+//    return BoundingBox(Coordinates(xExtremes.first->x + _deltaX, yExtremes.first->y + _deltaY),
+//                       Coordinates(xExtremes.second->x + _deltaX, yExtremes.second->y + _deltaY));
+//  }
 
-    return BoundingBox(Coordinates(xExtremes.first->x + _deltaX, yExtremes.first->y + _deltaY),
-                       Coordinates(xExtremes.second->x + _deltaX, yExtremes.second->y + _deltaY));
-  }
-
-  bool collideWith(BoundingBox bb) const {
-    BoundingBox bb1 = getBoundingBox();
-    BoundingBox bb2 = bb;
-
-    return bb1.upperLeft.x > bb2.lowerRight.x && bb1.lowerRight.x < bb2.upperLeft.x &&
-           bb1.upperLeft.y > bb2.lowerRight.y && bb1.lowerRight.y < bb2.upperLeft.y;
-  }
-
-  bool collideWith(const Shape &shape) const {
-    return collideWith(shape.getBoundingBox());
-  }
-  
-  static bool collide(const Shapes shapes1, const Shapes shapes2) {
-    for (const auto &shape1: shapes1) {
-      for (const auto &shape2: shapes2) {
-        if (shape1.collideWith(shape2)) {
-          return  true;
-        }
-      }
-    }
-    return false;
-  }
+//  bool collideWith(BoundingBox bb) const {
+//    BoundingBox bb1 = getBoundingBox();
+//    BoundingBox bb2 = bb;
+//
+//    return bb1.upperLeft.x > bb2.lowerRight.x && bb1.lowerRight.x < bb2.upperLeft.x &&
+//           bb1.upperLeft.y > bb2.lowerRight.y && bb1.lowerRight.y < bb2.upperLeft.y;
+//  }
+//
+//  bool collideWith(const Shape &shape) const {
+//    return collideWith(shape.getBoundingBox());
+//  }
+//  
+//  static bool collide(const Shapes shapes1, const Shapes shapes2) {
+//    for (const auto &shape1: shapes1) {
+//      for (const auto &shape2: shapes2) {
+//        if (shape1.collideWith(shape2)) {
+//          return  true;
+//        }
+//      }
+//    }
+//    return false;
+//  }
 
 };
 
